f096c62321809cf544f3381435ad1d2a8d89f408
Danish Chandargi <danish.chandargi@uptiq.ai>
2026-01-19 15:27:03 +0530

    remove trailing slashes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin:"https://globe-guide-ai.vercel.app/",
+  origin:"https://globe-guide-ai.vercel.app",
   methods:["POST"],
   credentials:true,
 }));
